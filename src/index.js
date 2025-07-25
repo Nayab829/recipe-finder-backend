6ef3fda0ec3f826458e18210d1480bc0d31b2f57
nayab829 <nayabg829@gmail.com>
2025-07-25 10:13:39 +0500

    added cors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import express from "express"
 import 'dotenv/config'
 import recipeRouter from "../routes/recipe.routes.js"
 import connectDB from "../config/db.js";
+import cors from "cors"
 const app = express();
-// middelewares
 
+
+// middelewares
+app.use(cors())
 app.use(express.json())
 
 
