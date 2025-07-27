61c8ccd3f3fe44373058b726859f783d87ecc33f
nayab829 <nayabg829@gmail.com>
2025-07-27 16:30:15 +0500

    fix file structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import 'dotenv/config'
-import recipeRouter from "../routes/recipe.routes.js"
-import connectDB from "../config/db.js";
+import recipeRouter from "./routes/recipe.routes.js"
+import connectDB from "./config/db.js";
 import cors from "cors"
 const app = express();
 
