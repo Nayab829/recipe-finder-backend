f85fc5938a54f0d4fb5bd6358b21703811de9157
nayab829 <nayabg829@gmail.com>
2025-07-27 15:50:48 +0500

    added footer and navbar

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ connectDB()
 
 app.use("/favourites",recipeRouter)
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 
